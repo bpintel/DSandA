7d896480cfe93a402dd29d1ab2d29df1b9106beb
Ben Pintel <bpintel@gmail.com>
2022-03-18 15:05:55 -0700

    radix sort notes

--- sorting/radixSort.js
+++ sorting/radixSort.js
@@ -1,4 +1,4 @@
-//TC: O__
+//TC: O(nk) where k is the number of digits. (k is almost always log n because of how computers store numbers)
 
 
 
@@ -34,3 +34,33 @@ const test2 = [1, 23, 455, 31234, 23];
 console.log('getDigit: ', getDigit(test, 2)); //2
 console.log('digitCount: ', digitCount(test));  //4
 console.log('mostDigits: ', mostDigits(test2)); //5
+
+//radix sort
+
+//input: list of numbers
+//output: sorted list of numbers
+//find how many digits the largest number has
+//loop from n=0 up to the largest number of digits
+//for each iteration
+  //create buckets for each digit (0 to 9)
+  //place each number in the corresponding bucket based on its nth digit
+  //replace our existing array with values in our buckets starting with 0 going up to 9
+//return list
+
+const radixSort = (numbers) => {
+  let maxDigitCount = mostDigits(numbers);
+  for (let k = 0; k < maxDigitCount; k++){
+    let digitBuckets = Array.from({length:10}, () => []);
+    for (let i = 0; i < numbers.length; i++) {
+      let digit = getDigit(numbers[i], k);
+      digitBuckets[digit].push(numbers[i]);
+    }
+    numbers = [].concat(...digitBuckets);
+  }
+  return numbers;
+}
+
+
+const test3 = [23,345,5467,12,2345,9852];
+console.log('radixSort: ', radixSort(test3)) //[12, 23, 345, 2345, 5467, 9852]
+
