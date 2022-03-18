841adc5e5da105ceabe45c514378af5131e3eec5
Ben Pintel <bpintel@gmail.com>
2022-03-18 12:11:28 -0700

    merge sort notes

--- sorting/mergeSort.js
+++ sorting/mergeSort.js
@@ -4,6 +4,9 @@
 
 
 // needs to run in O(n+m) and not modify the input arrays
+
+//first merge two sorted arrays
+
 //create an empty array, take a look at the smallest values in each input array
 //while there are still values we haven't looked at
   //if the value in the first array is smaller than the value in the second array, 
@@ -15,16 +18,47 @@
   //once we exhaust one array, push in all remaining values from the other array
 
 const merge = (arr1, arr2) => {
-  let result = [];
-
-  
-
-
-  return result;
+  let results = [];
+  let i = 0;
+  let j = 0;
+  while (i < arr1.length && j < arr2.length){
+    if (arr2[j] > arr1[i]){
+      results.push(arr1[i]);
+      i++;
+    } else {
+      results.push(arr2[j]);
+        j++;
+    }
+  }
+  while(i < arr1.length){
+    results.push(arr1[i]);
+    i++;
+  }
+  while(j < arr2.length){
+    results.push(arr2[j]);
+    j++;
+  }
+  return results;
 }
 
 const test1 = [1, 10, 50];
 const test2 = [2, 14, 99, 100];
 
 
-console.log(merge(test1, test2)) // [1, 2, 10, 14, 50, 99, 100]
+// console.log(merge(test1, test2)) // [1, 2, 10, 14, 50, 99, 100]
+
+
+//merge sort 
+
+//break up the array into halves until you have arrays that are empty or have one el
+//then merge the smaller arrays into a sorted array with merge func.
+
+const mergeSort = (arr) => {
+  if(arr.length <= 1) return arr;
+  let mid = Math.floor(arr.length /2);
+  let left = mergeSort(arr.slice(0, mid));
+  let right = mergeSort(arr.slice(mid));
+  return merge(left,right)
+}
+
+console.log(mergeSort([10,24,76,73,72,1,9]))
